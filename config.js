51360252f5c1163448654811ad1b73b1e24978df
Obinna Nnodim <obiknows88@gmail.com>
2020-02-21 00:30:15 -0700

    updated the config copy

--- config.js
+++ config.js
@@ -1,37 +1,32 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
-  manifestName: 'Eventually',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'FIRE Resorts', // <title>
+  manifestName: 'FIRE Resorts',
+  manifestShortName: 'FIRE Resorts', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  manifestIcon: 'src/assets/img/fireresorts-fire.png',
+  // pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Eventually',
+  heading: 'FIRE RESORTS',
   subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
+    "FIRE Resorts is finally here. The space for Like Minded Nomads of Color to Meet, Host, Rest & VIBE",
   socialLinks: [
     {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://instagram.com/twip.s',
     },
-    {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
-    },
+    // {
+    //   icon: 'fa-facebook',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/theanubhav',
+    // },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:fireresorts@gmail.com',
     },
   ],
 };
